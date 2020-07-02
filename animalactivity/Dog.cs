7fb8bfcd0dc0987b4de34f60f2ec725fc9f08fd9
Navpreet Kaur <NK028@atcltd.atc.org.nz>
2020-07-03 08:38:38 +1200

    Add project files.

---
+++ animalactivity/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace animalactivity
+{
+    public class Dog:Animal
+    {
+        public override string Name { get; set; }
+        public override PictureBox Picture { get; set; }
+        public Dog(string temName, PictureBox tempic)
+        {
+            Name = temName;
+            Picture = tempic;
+        }
+        public override void Move(int distance)
+        {
+            Picture.Location = new Point(Picture.Location.X + distance, Picture.Location.Y);
+        }
+
+    }
+}
